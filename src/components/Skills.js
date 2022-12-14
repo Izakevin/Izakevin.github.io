8a7b8a18f6b08a35249f6c6ba7031021c053cf70
Kevin <keviniza64@gmail.com>
2022-12-14 13:09:47 -0500

    Updated Education layout

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -29,8 +29,7 @@ function Skills(){
             <li><Docker/></li>
             <li><MySQL/></li>
             <li><MondoDB/></li>
-        </ul>
-        
+        </ul>        
     </div>
     )
 }
