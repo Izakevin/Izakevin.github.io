9cffe3c08ff0377876dc2b069c1d0744ea1b7256
Kevin <keviniza64@gmail.com>
2022-12-14 12:21:26 -0500

    updated About Me content

--- src/components/About_Me.js
+++ src/components/About_Me.js
@@ -4,7 +4,11 @@ function AboutMe(){
     return (
         <div id="about_me">
           <h2 className='section'>About Me</h2>
-          <p>I am Kevin Iza a software developer from Brooklyn New York. My interest in technology started when I realized I had witnessed a shift in the portable phone market from flip and sliding phones to smartphones with touch displays. The rapid change and adoption were fascinating to see and look back on.
+          <p>
+          Hello World! I’m Kevin Iza a Software Developer based in Brooklyn, New York. I have experience in Front-end development as well as application development. 
+          <br></br>
+          <br></br>
+          My interest in coding began when I witnessed firsthand the shift from flip and sliding phones to smartphones. Looking back the rate at which the market changed and adopted new technology inspired me to want to create and participate in this new wave of technology.
           </p>
         </div>
     )
