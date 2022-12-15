238860eb37a774baf4f143b821636045ab71a4d5
Kevin <keviniza64@gmail.com>
2022-12-14 20:17:31 -0500

    Added a Project and MUI framework

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
+// import Button from '@mui/material/Button';
 
 import Header from './components/Header.js';
 import Education from './components/Education.js';
 import Skills from './components/Skills.js';
 import Contact from './components/Contact';
 import AboutMe from './components/About_Me';
+// import DrawerAppBar from './components/DrawerAppBar';
 
 function App() {
 
   return (
     <div>
+      {/* <DrawerAppBar/> */}
       <div id="app_header">
         <Header/>
       </div>
@@ -22,7 +25,13 @@ function App() {
         <div id="projects">
           <h2 className='section'>My Projects</h2>
 
-          <h3>Coming Soon ...</h3>
+          <div id = "project">
+            <h3>Only Survive</h3>
+            <p>Coded in C++</p>
+            <p>Only Survive is a first-person shooter survival game with the only goal is surviving. Enemies will spawn and chase you, be ready to wipe them out to survive or outrun them to run out the clock.</p>
+            <p>This project features custom-made sprites I created for the enemies and for HUD, and incorporates music and sound effects when shooting the fireballs.</p>
+          </div>
+
         </div>
 
         <Skills/>
