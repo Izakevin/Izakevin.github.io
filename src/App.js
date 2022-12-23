acf7c83de88f3f237cc9d1008752417fffd16c18
Kevin <keviniza64@gmail.com>
2022-12-23 17:18:20 -0500

    Seperated Projects into its own component and moved the styling to Projects.css

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Education from './components/Education.js';
 import Skills from './components/Skills.js';
 import Contact from './components/Contact';
 import AboutMe from './components/About_Me';
+import Projects from './components/Projects';
 // import DrawerAppBar from './components/DrawerAppBar';
 
 function App() {
@@ -22,17 +23,7 @@ function App() {
 
         <Education/>
 
-        <div id="projects">
-          <h2 className='section'>My Projects</h2>
-
-          <div id = "project">
-            <h3>Only Survive</h3>
-            <p>Coded in C++</p>
-            <p>Only Survive is a first-person shooter survival game with the only goal is surviving. Enemies will spawn and chase you, be ready to wipe them out to survive or outrun them to run out the clock.</p>
-            <p>This project features custom-made sprites I created for the enemies and for HUD, and incorporates music and sound effects when shooting the fireballs.</p>
-          </div>
-
-        </div>
+        <Projects/>
 
         <Skills/>
 
---
+++ src/components/Projects.js
@@ -0,0 +1,19 @@
+import './styles/Projects.css'
+
+function Projects(){
+    return (
+        <div id="projects">
+          <h2 className='section'>My Projects</h2>
+
+          <div id = "proj">
+            <h3>Only Survive</h3>
+            <p>Coded in C++</p>
+            <p>Only Survive is a first-person shooter survival game with the only goal is surviving. Enemies will spawn and chase you, be ready to wipe them out to survive or outrun them to run out the clock.</p>
+            <p>This project features custom-made sprites I created for the enemies and for HUD, and incorporates music and sound effects when shooting the fireballs.</p>
+          </div>
+
+        </div>
+    )
+}
+
+export default Projects;
